65a296522e8ac74f54a8f5103dfb35ad8ace8839
dffxPipeline <rmacaluso@digitalfrontierfx.com>
2023-04-19 18:53:46 -0700

    changes to container for attempts at better collapse

--- Container.py
+++ Container.py
@@ -10,7 +10,7 @@ if sys.version_info.major == 3:
 class Header(QWidget):
     """Header class for collapsible group"""
 
-    def __init__(self, name, content_widget):
+    def __init__(self, main_layout, main_scroll, name, content_widget):
         """Header Class Constructor to initialize the object.
 
         Args:
@@ -18,6 +18,8 @@ class Header(QWidget):
             content_widget (QtWidgets.QWidget): Widget containing child elements
         """
         super(Header, self).__init__()
+        self.main_layout = main_layout
+        self.main_scroll = main_scroll
         self.content = content_widget
         self.expand_ico = QPixmap(":teDownArrow.png")
         self.collapse_ico = QPixmap(":teRightArrow.png")
@@ -34,7 +36,7 @@ class Header(QWidget):
         self.icon = QLabel()
         self.icon.setPixmap(self.expand_ico)
         self.layout.addWidget(self.icon)
-        self.layout.setContentsMargins(11, 0, 11, 0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
 
         font = QFont()
         font.setBold(True)
@@ -60,6 +62,15 @@ class Header(QWidget):
         self.content.setVisible(False)
         self.icon.setPixmap(self.collapse_ico)
 
+    # def updateSize(self):
+    #     left, top, right, bottom, = self.content.getContentsMargins()
+    #     hint = self.content.sizeHint()
+    #     print (hint)
+    #     self.main_scroll.setMaximumHeight(int((hint.height() + top + bottom + 1) * 1.5))
+    #     self.main_scroll.setMinimumHeight(int((hint.height() + top + bottom + 1) * 1.5))
+    #     self.main_scroll.setMinimumWidth(int((hint.width() + left + right) * 2))
+    #     self.main_scroll.setMaximumWidth(int((hint.width() + left + right) * 2))
+    #     #print ("setMaximumHeight: %s\n" % (str(hint.height() + top + bottom + 1)))
 
 class Container(QWidget):
     """Class for creating a collapsible group similar to how it is implement in Maya
@@ -73,7 +84,7 @@ class Container(QWidget):
             >>> content_layout = QtWidgets.QGridLayout(container.contentWidget)
             >>> content_layout.addWidget(QtWidgets.QPushButton("Button"))
     """
-    def __init__(self, name, color_background=False):
+    def __init__(self, main_layout, main_scroll, name, color_background=False):
         """Container Class Constructor to initialize the object
 
         Args:
@@ -81,15 +92,15 @@ class Container(QWidget):
             color_background (bool): whether or not to color the background lighter like in maya
         """
         super(Container, self).__init__()
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
+        container_layout = QVBoxLayout(self)
+        container_layout.setContentsMargins(0, 0, 0, 0)
         self._content_widget = QWidget()
         if color_background:
             self._content_widget.setStyleSheet(".QWidget{background-color: rgb(73, 73, 73); "
                                                "margin-left: 2px; margin-right: 2px}")
-        self.header = Header(name, self._content_widget)
-        layout.addWidget(self.header)
-        layout.addWidget(self._content_widget)
+        self.header = Header(main_layout, main_scroll, name, self._content_widget)
+        container_layout.addWidget(self.header)
+        container_layout.addWidget(self._content_widget)
 
         # assign header methods to instance attributes so they can be called outside of this class
         self.collapse = self.header.collapse
